27e767e599dc038fb535a4c7482c8c090b24a739
Collins Adom Baffour <collins.baffour@amalitech.org>
2021-06-17 16:49:59 +0000

    Update

--- js/script.js
+++ js/script.js
@@ -1,17 +1,14 @@
 let LEAP = () => {
-    let year = parseInt(document.getElementById('year').value)
-
-    //check if year is in range
-    if (year < 1 && year > 9999) {
-        document.getElementById('output').value = `Year out of range`
-    }
-    //year divisble by 4  or 100 or 400
-    else if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0) {
-        document.getElementById('output').value = ` ${year} is a leap year`
+    let year = parseInt(document.getElementById('year').value);
+    // //year divisble by 4  or 100 or 400
+    if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
+        document.getElementById('output').value = `${year}  is a leap year`;
+    } else if ((year < 1) && (year > 9999)) {
+        document.getElementById('output').value = ` ${year} not in range`;
     } else {
-        document.getElementById('output').value = `Your BMI is ${year}, so you are extremely obese.`
+        document.getElementById('output').value = `${year} 
+        is not a leap year`;
     }
-
 }
 let CLR = () => {
     document.getElementById('year').value = ``
